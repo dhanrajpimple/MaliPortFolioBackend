3d37ddee90467f77d16bfb9c20dd47b2adca8b31
dhanrajpimple <dhanrajpimple16012001@gmail.com>
2025-01-31 21:39:13 +0530

    sd

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 app.use(
     cors({
-      origin: 'http://localhost:5173', // Replace with your frontend's URL
+      origin: 'https://backend-mali.vercel.app/', // Replace with your frontend's URL
       credentials: true, // Allow cookies to be sent with requests
     })
   );
